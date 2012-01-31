efc313fc3b54a714f02abfeaceab39b5a61e92c4
Mikestro <miekstro@gmail.com>
2012-01-30 18:03:20 -0800

    Added an x86 Linux executable, and added the source files for the Map class which were not pushed in the last few commits..

---
+++ source/Map.cpp
@@ -0,0 +1,50 @@
+﻿#include "Map.hpp"
+
+Map::Map(string filename,Screen* s,vector<Sprite*> *spr){
+	screen = s;
+	sprites = spr;
+	std::ifstream map(filename.c_str());
+	int x = 0, y = 0;
+	
+	for(int t = 0; t < TILES_PER_SCREEN; t++){
+		int tileType = -1;
+		map >> tileType;
+		
+		if((tileType >= 0)&&(tileType < TOTAL_TILES)){
+			tiles.push_back(new Tile(screen,(*sprites)[1],tileType,x,y));
+		}
+		
+		x += TILE_WIDTH;
+		
+		if(x >= 640){
+			x = 0;
+			y += TILE_HEIGHT;
+		}
+	}
+	
+	/*std::cout << vTiles.max_size() << std::endl;
+	std::cout << vTiles.size() << std::endl;*/
+	
+	map.close();
+	
+	
+}
+
+Map::~Map(){
+	tiles.clear();
+}
+
+void Map::render(){
+	for(int i = 0; i < tiles.size(); i++){
+		tiles[i]->render();
+	}
+}
+
+bool Map::isCollide(Entitiy *other){
+	for(i=0; i<tiles.size();i++){
+		if((other->isCollisionWith(tiles[i]))&&(tiles[i].gType == WATER))
+			return true;
+	}
+	
+	return false;
+}
---
+++ source/Map.hpp
@@ -0,0 +1,29 @@
+﻿#ifndef MAP_HPP
+#define MAP_HPP
+
+#include "Tile.hpp"
+#include "Sprite.hpp"
+#include "Entity.hpp"
+#include <vector>
+#include <string>
+using std::vector;
+using std::string;
+
+class Map{
+	private:
+		vector<Tile*> tiles;
+		vector<Sprite*> *sprites;
+		Screen *screen;
+	public:
+		Map(string,Screen*,vector<Sprite*>*);
+		~Map();
+		
+		void render();
+		//void reload();
+		
+		bool isCollide(Entity*);
+		
+		vector<Tile*> *getTiles();
+};
+
+#endif
